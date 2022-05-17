5809b63b9c70abff47181cd8a2aa117e7d17d677
J_M_Dominguez <jmdominguezhernandez@gmail.com>
2022-05-18 01:02:35 +0200

    Actualizacion 1705.03

--- CS Xampp/GestorLogisticaInversa/PHPServidor.php
+++ CS Xampp/GestorLogisticaInversa/PHPServidor.php
@@ -1,8 +1,9 @@
 <?php
-    if(isset($_POST["PickUp_ID"])){
-        $PickUp_ID = $_POST["PickUp_ID"];
+    if(isset($_POST["Customer_ID"])){
+        
+       /*  $PickUp_ID = $_POST["PickUp_ID"]; */
         $Customer_ID = $_POST["Customer_ID"];
-        $ID_User = $_POST["ID_User"];
+       /*  $ID_User = $_POST["ID_User"];
         $AssociatedOrder_ID = $_POST["AssociatedOrder_ID"];
         $DateRequest = $_POST["DateRequest"];
         $PreferentialTimetable = $_POST["PreferentialTimetable"];
@@ -10,33 +11,72 @@
         $MerchandiseRemarks = $_POST["MerchandiseRemarks"];
         $Department_ID = $_POST["Department_ID"];
         $Agency_ID = $_POST["Agency_ID"];
-        $PickUpRemarks = $_POST["PickUpRemarks"];
+        $PickUpRemarks = $_POST["PickUpRemarks"]; */
         
         $servidor = "localhost";
         $usuario = "root";
         $password = "";
-        $dbname = "reverselogisticsmanager";
+        $dbname = "reverselogisticsmng";
 
         $conexion = mysqli_connect($servidor, $usuario, $password, $dbname);
         if (!$conexion) {
             echo(alert("Fallo en la conexion"));
             echo "MySQL connection error: ".mysqli_connect_error();
-            
             exit();
-        } else {
-            echo("Conexion realizada");
-        }
 
-        $sql = "INSERT INTO pickups (PickUp_ID, Customer_ID, ID_User, AssociatedOrder_ID, DateRequest, PreferentialTimetable, Urgently, MerchandiseRemarks, Department_ID, Agency_ID, PickUpRemarks)
+        } else {
+            echo("Conexion establecida correctamente.");
+        }
+        $sql = "SELECT * FROM customers WHERE Customer_ID = $Customer_ID" ;      
+        $select = mysqli_query($conexion, $sql);
+        while ($dat=mysqli_fetch_assoc($select)){
+            $arr[]=$dat;
+        }
+        mysqli_close($conexion);
+
+        echo json_encode($arr);
+
+       /*  $sql = "INSERT INTO pickups (PickUp_ID, Customer_ID, ID_User, AssociatedOrder_ID, DateRequest, PreferentialTimetable, Urgently, MerchandiseRemarks, Department_ID, Agency_ID, PickUpRemarks)
         VALUES ('".addslashes($PickUp_ID)."','".addslashes($Customer_ID)."','".addslashes($ID_User)."', 
         '".addslashes($AssociatedOrder_ID)."','".addslashes($DateRequest)."', '".addslashes($PreferentialTimetable)."', 
         '".addslashes($Urgently)."','".addslashes($MerchandiseRemarks)."', 
-        '".addslashes($Department_ID)."','".addslashes($Agency_ID)."','".addslashes($PickUpRemarks)."')";
-       
-            if (mysqli_query($conexion, $sql)) {
-            echo "Record inserted correctly.";
+        '".addslashes($Department_ID)."','".addslashes($Agency_ID)."','".addslashes($PickUpRemarks)."')"; */
+              
+       /*  if (mysqli_query($conexion, $sql)) {
+            echo "\nRegistros guardados.";
+            echo json_encode($list);
+            include ('$list');
+
         } else {
-            echo "Errorg: ".mysqli_error($conexion);
-        }
+            echo "Error: ".mysqli_error($conexion);
+        } */
     }
+    /* else if(isset($_POST["Customer_ID"])){
+       
+        $Customer_ID = $_POST["Customer_ID"];
+        
+        $servidor = "localhost";
+        $usuario = "root";
+        $password = "";
+        $dbname = "reverselogisticsmng";
+
+        $conexion = mysqli_connect($servidor, $usuario, $password, $dbname);
+        
+        if (!$conexion) {
+            echo(alert("Fallo en la conexion"));
+            echo "MySQL connection error: ".mysqli_connect_error();
+            exit();
+        } else {
+            echo("Conexion establecida correctamente.");
+        }
+
+        $sql = "SELECT * FROM customers WHERE Customer_ID = 1000001";
+
+        if (mysqli_query($conexion, $sql)) {
+            echo "\nRegistros enviados.";
+        } else {
+            echo "Error: ".mysqli_error($conexion);
+        }
+    } */
+
 ?>
--- CS Xampp/GestorLogisticaInversa/src/pickups.js
+++ CS Xampp/GestorLogisticaInversa/src/pickups.js
@@ -1,14 +1,21 @@
 const open = document.getElementById('open');
 const modal_container = document.getElementById('modal_container');
 const close = document.getElementById('close');
-const exitform = document.getElementById('exitform');
+const exit = document.getElementById('cancel');
+
 
 //Abre ventana emergente para indicar nº cliente
 open.addEventListener('click', () => {
     modal_container.classList.add('show');
     document.getElementById("cliente").focus();
     document.getElementById("open").style.visibility = "hidden";
+    // datosCliente();
 });
+
+//Cancela el formulario y sale al principio (No deja volver a acceder al formulario)
+var cancel = () => {
+    window.location.replace("../forms/pickups.html");
+}
 
 
 //Captura nº cliente, cierra y esconde la ventana y hace visible el formulario
@@ -17,7 +24,7 @@ close.addEventListener('click', () => {
 
     const cliente_ = document.getElementById('cliente').value;
     //Validación del número
-    if (comprobarNumero(cliente_) == true) {
+    if (comprobarNumero(cliente_, 7) == true) {
         document.getElementById('codigo-cliente').value = cliente_;
         document.getElementById('cliente').value = "";
         modal_container.classList.remove('show');
@@ -27,7 +34,7 @@ close.addEventListener('click', () => {
         document.getElementById('limpiarformulario').style.visibility = "visible";
         document.getElementById('salirformulario').style.visibility = "visible";
         document.getElementById("pedido-asociado").focus();
-        ;
+
     } else {
         document.getElementById('cliente').value = "";
         alert('Introduzca un número correcto');
@@ -35,6 +42,7 @@ close.addEventListener('click', () => {
     }
 });
 
+//Añade ceros a los parámetros de la fecha cuando son menores de 10
 var updatefechahora = (datetime) => {
     if (datetime < 10) {
         return "0" + datetime;
@@ -46,16 +54,16 @@ var updatefechahora = (datetime) => {
 //Composición número de recogida >> (Varchar 25)
 var generarNumeroRecogida = () => {
     var date = new Date();
-    var CompanyClientCode = "46258"; //Debe venir de la tabla buscando por código de agencia
+    var CompanyClientCode = "4625"; //Debe venir de la tabla buscando por código de agencia
     var day = updatefechahora(date.getDate()).toString();
     var mounth = updatefechahora((date.getMonth() + 1)).toString();
     var year = date.getFullYear().toString();
     var hour = updatefechahora(date.getHours()).toString();
     var minute = updatefechahora(date.getMinutes()).toString();
     var customer = document.getElementById("codigo-cliente").value.toString();
+    var preferentialTime = document.getElementById('horario-recogida').value.toString();
     var urgent = document.getElementById('urgente').value.toString();
-    var PickUp_ID = CompanyClientCode + year + mounth + day + hour + minute + customer + urgent;
-
+    var PickUp_ID = CompanyClientCode + year + mounth + day + hour + minute + customer + preferentialTime + urgent;
     /*  console.log("PickUp_ID " + PickUp_ID);
         console.log("Tamaño PickUp_ID " + PickUp_ID.length); */
 
@@ -69,35 +77,34 @@ var generarNumeroRecogida = () => {
 
 /*VERIFICACIÓN Fecha de recogida. Compara la fecha actual con la de la solicitud 
 y si es para hoy o anterior nos muestra un error, sólo las habilitamos para el día siguiente.*/
-var comprobarfecha = (fechaSolicitud) => {
+var comprobarFecha = (fechaSolicitud) => {
     var date = new Date();
     var day = updatefechahora(date.getDate());
     var mounth = updatefechahora(date.getMonth() + 1);
     var year = date.getFullYear();
-    var fechaActual = new Date(fecha = year + "-" + mounth + "-" + day);
+    var fechaActual = year + "-" + mounth + "-" + day;
     var diferenciaFechas = parseInt(new Date(fechaActual).getTime()) - parseInt(new Date(fechaSolicitud).getTime());
-
-    /*  console.log("Diferencia1: " + diferenciaFechas);
-        console.log("fecha actual" + fechaActual);
-        console.log("Fecha solicitud de sde comprobar:" + fechaSolicitud); */
-
-    var fechaOK;
-    if (diferenciaFechas >= 0) {
-        fechaOK = false;
-        return fechaOK;
+    var numDay = new Date(fechaSolicitud).getDay();
+    /* console.log("Diferencia: " + diferenciaFechas);
+    console.log("Fecha actual: " + fechaActual);
+    console.log("hoy:" + numDay); */
+    if (diferenciaFechas >= 0 || numDay == 0 || numDay == 6) {
+        return false;
     } else {
-        return fechaOK;
+        return true;
     }
 }
 
 //Comprobación números
-var comprobarNumero = (numero) => {
+var comprobarNumero = (numero, long) => {
     let num = "";
+    let cont = 0;
     for (let i = 0; i < numero.length; i++) {
         if (numero.charCodeAt(i) <= 57 && numero.charCodeAt(i) >= 48) //ASCII numeros
             num = num + numero.charAt(i);
+        cont = cont + 1;
     }
-    if (num === numero) return true;
+    if (num === numero && cont === long) return true;
     else return false;
 }
 
@@ -105,21 +112,27 @@ var comprobarNumero = (numero) => {
 var enviarsolicitud = () => {
 
     var PickUp_ID = generarNumeroRecogida();
-    var Customer_ID = document.getElementById("codigo-cliente").value;
+    var Customer_ID = document.getElementById("codigo-cliente").value;  //Ya validado
     var ID_User = "1";
+    //Validación número
     var AssociatedOrder_ID = document.getElementById("pedido-asociado").value;
-
-
-    //TO-DO: verificar validación de fecha
+    if (comprobarNumero(AssociatedOrder_ID, 6) == false) {
+        alert('Introduzca un Nº de pedido válido');
+        document.getElementById("pedido-asociado").value = "";
+        document.getElementById("pedido-asociado").focus;
+        return;
+    }
+    //Validación fecha (No puede ser anterior a hoy, )
     var DateRequest = document.getElementById("fecha-recogida").value;
-    /* if (comprobarfecha(DateRequest) == true) {
-         DateRequest = document.getElementById("fecha-recogida").value;
-     }
-     else {
-         alert('Elija una fecha valida');
-         document.getElementById("fecha-recogida").value = "";
-     }; */
-
+    if (comprobarFecha(DateRequest) == true) {
+        DateRequest = document.getElementById("fecha-recogida").value;
+    }
+    else {
+        alert('Elija una fecha valida');
+        document.getElementById("fecha-recogida").value = "";
+        document.getElementById("fecha-recogida").focus;
+        return;
+    };
 
     var PreferentialTimetable = document.getElementById('horario-recogida').value;
     var Urgently = document.getElementById('agencia-transporte').value;
@@ -128,14 +141,6 @@ var enviarsolicitud = () => {
     var Agency_ID = document.getElementById("agencia-transporte").value;
     var PickUpRemarks = document.getElementById("observaciones-recogidas").value;
 
-
-    //Validacione números
-    if (comprobarNumero(AssociatedOrder_ID) == false) {
-        alert('Introduzca un Nº de pedido válido');
-        document.getElementById("pedido-asociado").value = "";
-        document.getElementById("pedido-asociado").focus;
-        return;
-    }
 
     //Comprobación por consola
     console.log("Customer_ID: " + Customer_ID);
@@ -159,11 +164,11 @@ var enviarsolicitud = () => {
     //enviamos datos al servidor con ajax
     $.ajax({
         type: "POST",
-        url: "../PHPServidor.php",
+        url: "../PHPServidor.php",  //dirección del servidor
         data: {
-            PickUp_ID: PickUp_ID,
+            /* PickUp_ID: PickUp_ID, */
             Customer_ID: Customer_ID,
-            ID_User: ID_User,
+            /* ID_User: ID_User,
             AssociatedOrder_ID: AssociatedOrder_ID,
             DateRequest: DateRequest,
             PreferentialTimetable: PreferentialTimetable,
@@ -171,10 +176,31 @@ var enviarsolicitud = () => {
             MerchandiseRemarks: MerchandiseRemarks,
             Department_ID: Department_ID,
             Agency_ID: Agency_ID,
-            PickUpRemarks: PickUpRemarks
+            PickUpRemarks: PickUpRemarks */
         },
+
         success: function (response) {
-            alert(response)
+    
+            var index = response.indexOf("[");
+            var data2 = response.substring(index + 1, response.length - 1);
+            var json = data2.substring(0, data2.length-1);
+            var datosCliente = [];
+
+            console.log(data2);
+            var array_companyias = json.split(',');
+            for (i = 0; i < array_companyias.length; i++) {
+                console.log(array_companyias[i]);
+            }
+
+            for (j = 0; j < array_companyias.length; j++) {
+                var index2 = array_companyias[j].indexOf(":");
+                dato3 = array_companyias[j].substring(index2 + 2, array_companyias[j].length-1);
+                datosCliente.add(dato3);
+                console.log(j + " >>> " + dato3)
+            }
+
+            document.getElementById(datosCliente[1]).value.innerText;
+
         },
         error: function () {
             alert("Error");
@@ -182,17 +208,19 @@ var enviarsolicitud = () => {
     });
     document.getElementById('cont1').style.visibility = "hidden";
     document.getElementById('cont2').style.visibility = "hidden";
-    document.getElementById('borrarformulario').style.visibility = "hidden";
+    document.getElementById('limpiarformulario').style.visibility = "hidden";
+    document.getElementById('salirformulario').style.visibility = "hidden";
+    document.getElementById("open").style.visibility = "visible";
     reset();
-    alert("Se ha generado la recogida " + PickUp_ID)
-
-    /*TO-DO: Generar fichero para la agencia de transportes*/
+    alert("Se ha generado la recogida " + PickUp_ID);
+    guardarArchivoTexto(PickUp_ID, PickUp_ID);
+    alert("Se ha generado el fichero:" + PickUp_ID);
 }
 
 //Función para limpiar todos los campos
 var reset = () => {
     ID_User = "1";
-    document.getElementById("codigo-cliente").value = "";
+    //document.getElementById("codigo-cliente").value = "";
     document.getElementById("nombre-cliente").value = "";
     document.getElementById("direccion-cliente").value = "";
     document.getElementById("direccion-cliente2").value = "";
@@ -206,13 +234,20 @@ var reset = () => {
     document.getElementById("observaciones-mercancia").value = "";
     document.getElementById("fecha-recogida").value = "";
     document.getElementById('agencia-transporte').value = "";
-    document.getElementById('horario-recogida').innerHTML = "Tarde";
-    document.getElementById("urgente").value = "No urgente";
+    document.getElementById('horario-recogida').value = "1";
+    document.getElementById("urgente").value = "0";
     document.getElementById("observaciones-recogidas").value = "";
 }
 
-
-
-
+//Genera y rellena el fichero txt. Lo descarga automáticamente en "downloads";
+var guardarArchivoTexto = (contenido, nombre) => {
+    const a = document.createElement("a");
+    const archivo = new Blob([contenido], { type: 'text/plain' });
+    const url = URL.createObjectURL(archivo);
+    a.href = url;
+    a.download = nombre;
+    a.click();
+    URL.revokeObjectURL(url);
+}
 
 
